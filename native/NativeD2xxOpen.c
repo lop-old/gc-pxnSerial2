5c2328ea7e5d7f294b94a5a087c86251a91bcbd3
lorenzop <lorenzo@poixson.com>
2017-10-13 13:03:03 -0400

    finished natGetDeviceList() function in NativeD2xxOpen.c

--- native/NativeD2xxOpen.c
+++ native/NativeD2xxOpen.c
@@ -11,6 +11,13 @@
 
 #include <jni.h>
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <unistd.h>
+
 // open source library
 #include "ftdi/open/ftdi.h"
 
@@ -24,7 +31,75 @@
 JNIEXPORT jobjectArray JNICALL
 Java_com_poixson_serial_natives_NativeD2xxOpen_natGetDeviceList
 (JNIEnv *env, jobject obj) {
-return NULL;
+	struct ftdi_context* ftdi;
+	ftdi = ftdi_new();
+	if (ftdi == 0) {
+		fprintf(stderr, "ftdi_new() failed\n");
+		return NULL;
+	}
+	// get device serial numbers
+	struct ftdi_device_list* devList;
+	int result;
+	result = ftdi_usb_find_all(
+		ftdi,
+		&devList,
+		0, 0 // default vendor/product id's
+	);
+	if (result < 0) {
+		fprintf(stderr, "ftdi_usb_find_all() failed: %d (%s)\n", result, ftdi_get_error_string(ftdi));
+		ftdi_free(ftdi);
+		return NULL;
+	}
+	int count = result;
+	if (count > MAX_DEVICES) {
+		fprintf(stderr, "Found %d devices, more than max %d devices", count, MAX_DEVICES);
+		count = MAX_DEVICES;
+	}
+	struct ftdi_device_list* dev = devList;
+	char manu  [SERIAL_SIZE];
+	char desc  [SERIAL_SIZE];
+	char *list [MAX_DEVICES];
+	char serialBuf [MAX_DEVICES] [SERIAL_SIZE];
+	int index = 0;
+	int total = count;
+	while (dev!=NULL) {
+		result = ftdi_usb_get_strings2(
+			ftdi,
+			dev->dev,
+			manu,   SERIAL_SIZE,
+			desc,   SERIAL_SIZE,
+			serialBuf[index], SERIAL_SIZE
+		);
+		list[index] = serialBuf[index];
+		if (result < 0) {
+			fprintf(stderr, "ftdi_usb_get_strings2(%d) failed: %d, (%s)\n", index, result, ftdi_get_error_string(ftdi));
+			ftdi_list_free(&devList);
+			ftdi_free(ftdi);
+			return NULL;
+		}
+		total += strlen(list[index]);
+		dev = dev->next;
+		index++;
+	}
+	ftdi_list_free(&devList);
+	ftdi_free(ftdi);
+	// merge serial numbers to byte array
+	char resultBytes [total];
+	int pos = 0;
+	for (int i=0; i<count; i++) {
+		int len = strlen(list[i]);
+		memcpy(
+			resultBytes + pos,
+			list[i],
+			len
+		);
+		pos += len;
+		resultBytes[pos++] = 0;
+	}
+	// convert to java byte array
+	jbyteArray jbytes = (*env)->NewByteArray(env, total);
+	(*env)->SetByteArrayRegion(env, jbytes, 0, total, (jbyte*) resultBytes);
+	return jbytes;
 }
 
 
