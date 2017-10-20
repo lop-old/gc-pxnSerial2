41497de33bce7d67e2ea642a111ab4ba344d9543
lorenzop <lorenzo@poixson.com>
2017-10-19 20:37:59 -0400

    added some run() code to ExampleEcho class

--- java/src/com/poixson/serial/examples/ExampleEcho.java
+++ java/src/com/poixson/serial/examples/ExampleEcho.java
@@ -1,6 +1,9 @@
 package com.poixson.serial.examples;
 
+import com.poixson.serial.pxnSerial;
+import com.poixson.serial.pxnSerialFactory;
 import com.poixson.serial.enums.Baud;
+import com.poixson.utils.ThreadUtils;
 import com.poixson.utils.Utils;
 
 
@@ -13,6 +16,25 @@ public class ExampleEcho implements Runnable {
 
 	@Override
 	public void run() {
+		final String portName = this.getPortName();
+		final Baud   baud     = this.getBaud();
+		final pxnSerial serial =
+			pxnSerialFactory.get()
+				.setPortName(portName)
+				.setBaud(baud)
+				.build();
+		if (!serial.open()) {
+			System.out.println("Failed to open serial port: "+portName);
+			System.exit(1);
+		}
+
+		for (int i=0; i<10; i++) {
+			final String str = serial.readString();
+			System.out.println("GOT DATA: "+str);
+			ThreadUtils.Sleep(100L);
+		}
+
+		Utils.safeClose(serial);
 	}
 
 
