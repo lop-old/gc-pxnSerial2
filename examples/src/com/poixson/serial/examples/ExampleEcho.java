1551500bea6c4a65b8db825fd9dc3087c92c5670
lorenzop <lorenzo@poixson.com>
2017-10-29 07:32:15 -0400

    updated ExampleEcho class to use new AbstractExample class

--- examples/src/com/poixson/serial/examples/ExampleEcho.java
+++ examples/src/com/poixson/serial/examples/ExampleEcho.java
@@ -7,16 +7,13 @@ import com.poixson.utils.ThreadUtils;
 import com.poixson.utils.Utils;
 
 
-public class ExampleEcho implements Runnable {
-
-	private String portName = null;
-	private Baud   baud     = null;
+public class ExampleEcho extends AbstractExample {
 
 
 
 	@Override
 	public void run() {
-		System.out.println();
+		super.run();
 		final String portName = this.getPortName();
 		final Baud   baud     = this.getBaud();
 		final pxnSerial serial =
@@ -36,53 +33,6 @@ public class ExampleEcho implements Runnable {
 		}
 
 		Utils.safeClose(serial);
-		System.out.println();
-	}
-
-
-
-	// port name
-	public String getPortName() {
-		final String portName = this.portName;
-		return (
-			Utils.isEmpty(portName)
-			? "/dev/ttyUSB0"
-			: portName
-		);
-	}
-	public ExampleEcho setPortName(final String portName) {
-		this.portName = (
-			Utils.isEmpty(portName)
-			? null
-			: portName
-		);
-		return this;
-	}
-
-
-
-	// baud rate
-	public Baud getBaud() {
-		final Baud baud = this.baud;
-		return (
-			baud == null
-			? Baud.DEFAULT_BAUD
-			: baud
-		);
-	}
-	public ExampleEcho setBaud(final int baudValue) {
-		return this.setBaud(
-			Baud.FromInt(baudValue)
-		);
-	}
-	public ExampleEcho setBaud(final String baudStr) {
-		return this.setBaud(
-			Baud.FromString(baudStr)
-		);
-	}
-	public ExampleEcho setBaud(final Baud baud) {
-		this.baud = baud;
-		return this;
 	}
 
 
